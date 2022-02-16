77e311b63b6ff57e6d65265894d9e046e5a8ab8e
Intrets <geen.inspiratie824@gmail.com>
2022-02-16 21:54:17 +0100

    templated constructor for enum_bitflags

--- include/tepp/enum_bitflags.h
+++ include/tepp/enum_bitflags.h
@@ -29,13 +29,16 @@ namespace te
 		using value_type = std::underlying_type_t<Enum>;
 
 		value_type data{};
-		explicit enum_bitflags(value_type data_) : data(data_) {};
+		explicit enum_bitflags(value_type data_) : data(data_) {
+		};
 
 	public:
 		enum_bitflags() = default;
-		enum_bitflags(Enum e) {
-			this->data = static_cast<value_type>(e);
+
+		template<std::same_as<Enum>... Enums>
+		enum_bitflags(Enums... e) : enum_bitflags((static_cast<value_type>(e) | ...)) {
 		}
+
 		~enum_bitflags() = default;
 
 		constexpr enum_bitflags operator&(enum_bitflags other) const {
@@ -85,4 +88,7 @@ namespace te
 			*this &= ~enum_bitflags(d);
 		}
 	};
+
+	template<class T, class... Ts>
+	enum_bitflags(T, Ts...)->enum_bitflags<T>;
 }
