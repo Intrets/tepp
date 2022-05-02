a6e4c46b37987ae7a900538b03e6c8751042babc
Intrets <geen.inspiratie824@gmail.com>
2022-05-02 21:15:08 +0200

    removed unused duplicate StringLiteral

--- include/tepp/tepp.h
+++ include/tepp/tepp.h
@@ -22,16 +22,6 @@
 
 namespace te
 {
-	template<size_t N>
-	struct StringLiteral
-	{
-		constexpr StringLiteral(const char(&str)[N]) {
-			std::copy_n(str, N, value);
-		}
-
-		char value[N];
-	};
-
 	namespace detail
 	{
 		template<class T>
