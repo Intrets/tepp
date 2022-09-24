576b2bb573f20132f6890252a466e190f928ff3c
Intrets <geen.inspiratie824@gmail.com>
2022-09-24 12:18:11 +0200

    debug build type checking for cheated_storage

--- cheated_storage.cpp
+++ cheated_storage.cpp
@@ -1,5 +1,15 @@
 #include "tepp/cheated_storage.h"
 
+#if DEBUG_BUILD
+namespace te::debug
+{
+	int& typeCounter() {
+		static int counter = 0;
+		return counter;
+	}
+}
+#endif // DEBUG_BUILD
+
 namespace te
 {
 
--- include/tepp/cheated_storage.h
+++ include/tepp/cheated_storage.h
@@ -4,6 +4,25 @@
 
 #include <cassert>
 
+#if DEBUG_BUILD
+#include <mutex>
+
+namespace te::debug
+{
+	int& typeCounter();
+
+	template<class T>
+	static int getTypeIndex() {
+		static std::mutex mutex;
+
+		auto lock = std::scoped_lock(mutex);
+
+		static int typeIndex = typeCounter()++;
+		return typeIndex;
+	}
+}
+#endif // DEBUG_BUILD
+
 namespace te
 {
 	template <size_t size, size_t align = alignof(max_align_t)>
@@ -11,10 +30,19 @@ namespace te
 	{
 		alignas(align) std::byte storage[size];
 
+#if DEBUG_BUILD
+		int typeIndex = debug::getTypeIndex<void>();
+#endif // DEBUG_BUILD
+
 		template<class T, class... Args>
 		void init(Args&&... args) {
 			static_assert(sizeof(T) <= size);
 			static_assert(alignof(T) == align);
+
+#if DEBUG_BUILD
+			assert(this->typeIndex == debug::getTypeIndex<void>());
+			this->typeIndex = debug::getTypeIndex<T>();
+#endif // DEBUG_BUILD
 
 			::new(&this->storage) T(std::forward<Args>(args)...);
 		}
@@ -24,6 +52,15 @@ namespace te
 			static_assert(sizeof(T) <= size);
 			static_assert(alignof(T) == align);
 
+#if DEBUG_BUILD
+			if (this->typeIndex == debug::getTypeIndex<void>()) {
+				this->typeIndex = debug::getTypeIndex<T>();
+			}
+			else {
+				assert(this->typeIndex == debug::getTypeIndex<T>());
+			}
+#endif // DEBUG_BUILD
+
 			return *reinterpret_cast<T*>(&this->storage);
 		}
 
@@ -31,6 +68,15 @@ namespace te
 		T const& access() const {
 			static_assert(sizeof(T) <= size);
 			static_assert(alignof(T) == align);
+
+#if DEBUG_BUILD
+			if (this->typeIndex == debug::getTypeIndex<void>()) {
+				this->typeIndex = debug::getTypeIndex<T>();
+			}
+			else {
+				assert(this->typeIndex == debug::getTypeIndex<T>());
+			}
+#endif // DEBUG_BUILD
 
 			return *reinterpret_cast<T const*>(&this->storage);
 		}
