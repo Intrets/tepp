5cb07299bf85cfd4a3d48ef9d9e2686dda989fb0
Intrets <geen.inspiratie824@gmail.com>
2022-10-20 23:44:50 +0200

    small renaming

--- include/tepp/binary_semaphore.h
+++ include/tepp/binary_semaphore.h
@@ -8,31 +8,35 @@
 
 namespace te
 {
-	struct spin {};
-	struct wait {};
+	enum class blocking
+	{
+		spin,
+		wait
+	};
 
-	template<class type>
+	enum class users
+	{
+		one_consumer__one_producer
+	};
+
+	template<blocking blocking_type, users users_type>
 	struct binary_semaphore
 	{
 		std::atomic<unsigned char> counter;
 
 		void acquire() noexcept {
-			while (true) {
-				auto v = this->counter.exchange(0, std::memory_order_acquire);
-
-				if (v == 1) {
-					return;
-				}
-
-				if constexpr (std::same_as<type, wait>) {
-					this->counter.wait(0, std::memory_order_relaxed);
-				}
+			if constexpr (blocking_type == blocking::spin) {
+				while (this->counter.load(std::memory_order_relaxed) == 0) {}
 			}
+			else if constexpr (blocking_type == blocking::wait) {
+				this->counter.wait(0, std::memory_order_relaxed);
+			}
+			this->counter.store(0);
 		}
 
 		void release() noexcept {
-			this->counter.store(1, std::memory_order_release);
-			if constexpr (std::same_as<type, wait>) {
+			this->counter.store(1);
+			if constexpr (blocking_type == blocking::wait) {
 				this->counter.notify_one();
 			}
 		}
@@ -41,6 +45,7 @@ namespace te
 
 		binary_semaphore(unsigned char initial) : counter(initial) {
 			assert(initial == 0 || initial == 1);
+			static_assert(users_type == users::one_consumer__one_producer);
 		}
 		~binary_semaphore() = default;
 	};
