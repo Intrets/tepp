3e03d0c41ba48643739e45432c941c5dd042ebaf
Intrets <geen.inspiratie824@gmail.com>
2023-07-10 02:37:17 +0200

    parsing and toString for uuid

--- include/tepp/uuid.h
+++ include/tepp/uuid.h
@@ -9,12 +9,52 @@ namespace te
 {
 	struct uuid
 	{
-	private:
-		std::array<uint8_t, 16> data;
+		std::array<uint8_t, 16> data{};
 
-	public:
 		// 8-4-4-4-12 format: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 		static std::optional<uuid> fromString(std::string_view str);
+
+		constexpr static auto parseChar = [](char c) -> uint8_t {
+			if (c <= '9') {
+				return c - '0';
+			}
+			else if (c <= 'H') {
+				return 10 + c - 'A';
+			}
+			else {
+				return 10 + c - 'a';
+			}
+		};
+		constexpr static auto parseByte = [](char c1, char c2) -> uint8_t {
+			return parseChar(c1) * 16 + parseChar(c2);
+		};
+
+		constexpr uuid() = default;
+		constexpr uuid(char const (&str)[37]) {
+			if (str[8] == '-' && str[13] == '-' && str[18] == '-' && str[23] == '-') {
+				data[0] = parseByte(str[0], str[1]);
+				data[1] = parseByte(str[2], str[3]);
+				data[2] = parseByte(str[4], str[5]);
+				data[3] = parseByte(str[6], str[7]);
+
+				data[4] = parseByte(str[9], str[10]);
+				data[5] = parseByte(str[11], str[12]);
+
+				data[6] = parseByte(str[14], str[15]);
+				data[7] = parseByte(str[16], str[17]);
+
+				data[8] = parseByte(str[19], str[20]);
+				data[9] = parseByte(str[21], str[22]);
+
+				data[10] = parseByte(str[24], str[25]);
+				data[11] = parseByte(str[26], str[27]);
+				data[12] = parseByte(str[28], str[29]);
+				data[13] = parseByte(str[30], str[31]);
+				data[14] = parseByte(str[32], str[33]);
+				data[15] = parseByte(str[34], str[35]);
+			}
+		};
+		~uuid() = default;
 
 		std::string toString() const;
 
--- uuid.cpp
+++ uuid.cpp
@@ -17,10 +17,9 @@ namespace te
 		for (int i = 0; auto s : str | std::views::filter([](auto c) { return c != '-'; }) | std::views::chunk(2)) {
 			std::stringstream byte{};
 			for (auto c : s) {
-				byte << c;
+				result.data[i] <<= 4;
+				result.data[i] += parseChar(c);
 			}
-
-			byte >> result.data[i];
 
 			i++;
 		}
@@ -33,7 +32,53 @@ namespace te
 	}
 
 	std::string uuid::toString() const {
-		return "123";
+		std::stringstream result{};
+
+		auto constexpr convertToChar = [](uint8_t c) -> char {
+			if (c <= 9) {
+				return c + '0';
+			}
+			else {
+				return c - 10 + 'a';
+			}
+		};
+
+		auto writeByte = [&](auto i) {
+			auto c = this->data[i];
+			result.put(convertToChar(c >> 4));
+			result.put(convertToChar(c & 0xF));
+		};
+
+		writeByte(0);
+		writeByte(1);
+		writeByte(2);
+		writeByte(3);
+
+		result << '-';
+
+		writeByte(4);
+		writeByte(5);
+
+		result << '-';
+
+		writeByte(6);
+		writeByte(7);
+
+		result << '-';
+
+		writeByte(8);
+		writeByte(9);
+
+		result << '-';
+
+		writeByte(10);
+		writeByte(11);
+		writeByte(12);
+		writeByte(13);
+		writeByte(14);
+		writeByte(15);
+
+		return result.str();
 	}
 
 	std::size_t uuid::getHash() const {
