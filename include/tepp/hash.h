5545ba336e5bd07b566a67051d35af4155600066
Intrets <geen.inspiratie824@gmail.com>
2024-04-21 01:22:44 +0200

    only allow inspecting of bytes for te::hash when there is no padding

--- include/tepp/hash.h
+++ include/tepp/hash.h
@@ -1,33 +1,42 @@
 #pragma once
 
 #include <span>
+#include <tepp/tepp.h>
 
 namespace te
 {
-	template<class T>
+	namespace detail
+	{
+		template<class T>
+		concept no_padding = std::has_unique_object_representations_v<T>;
+	}
+
+	template<detail::no_padding... T>
 	struct hash_struct
 	{
-		size_t operator()(T const& data) const {
+		size_t operator()(T const&... data) const {
 			// FNV-1
 			constexpr size_t prime = 1099511628211ULL;
 			constexpr size_t offset = 14695981039346656037ULL;
 
 			size_t hash = offset;
 
-			auto constexpr byteSize = sizeof(T);
-			std::byte const* raw = reinterpret_cast<std::byte const*>(&data);
+			te::for_each([&hash](auto const& data) {
+				auto constexpr byteSize = sizeof(data);
+				std::byte const* raw = reinterpret_cast<std::byte const*>(&data);
 
-			for (auto byte : std::span(raw, byteSize)) {
-				hash *= prime;
-				hash ^= static_cast<size_t>(std::bit_cast<uint8_t>(byte));
-			}
+				for (auto byte : std::span<std::byte const>(raw, byteSize)) {
+					hash *= prime;
+					hash ^= static_cast<size_t>(std::bit_cast<uint8_t>(byte));
+				}
+			});
 
 			return hash;
 		}
 	};
 
-	template<class T>
-	size_t hash(T const& a) {
-		return hash_struct<T>()(a);
+	template<detail::no_padding... T>
+	size_t hash(T const&... a) {
+		return hash_struct<T...>()(a...);
 	}
 }
