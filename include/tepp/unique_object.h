6e0a62fa72856ede600075e51d5af9e3b212de5f
Intrets <geen.inspiratie824@gmail.com>
2024-06-04 20:40:27 +0200

    forgot files

---
+++ include/tepp/unique_object.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "tepp/misc.h"
+
+namespace te
+{
+	template<class T>
+	struct unique_object
+	{
+	private:
+		T value;
+
+	public:
+		NO_COPY(unique_object);
+
+		T* operator->() {
+			return &this->get();
+		}
+
+		T const* operator->() const {
+			return &this->get();
+		}
+
+		T& get() {
+			return this->value;
+		}
+
+		T const& get() const {
+			return this->value;
+		}
+
+		unique_object& operator=(unique_object&& other) {
+			this->value = std::move(other.value);
+			other.value = {};
+			return *this;
+		}
+
+		unique_object(unique_object&& other)
+		    : value(std::move(other.value)) {
+			other.value = {};
+		}
+
+		template<class... Args>
+		unique_object(Args&&... args)
+		    : value(std::forward<Args>(args)...) {
+		}
+
+		unique_object() = default;
+		~unique_object() = default;
+	};
+}
---
+++ unique_object.cpp
@@ -0,0 +1,5 @@
+#include "tepp/unique_object.h"
+
+namespace te
+{
+}
