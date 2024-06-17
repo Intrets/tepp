51fd34b9e862527fb2290a74f9608874fa6f55d6
Intrets <geen.inspiratie824@gmail.com>
2024-06-18 00:44:30 +0200

    integer_t literal

--- include/tepp/integers.h
+++ include/tepp/integers.h
@@ -9,3 +9,7 @@ using qualifier_t = int64_t;
 inline integer_t isize(auto&& a) {
 	return static_cast<integer_t>(a.size());
 }
+
+inline integer_t operator""_i(unsigned long long i) {
+	return static_cast<integer_t>(i);
+}
