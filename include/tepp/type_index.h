7dc531b9534d8d40d4fb6640e89e51aa849988df
Intrets <geen.inspiratie824@gmail.com>
2024-10-15 15:26:43 +0200

    actually correctly return the right type and use integer_t instead of ints

--- include/tepp/type_index.h
+++ include/tepp/type_index.h
@@ -4,19 +4,19 @@
 
 namespace te
 {
-	template<class T, class index_type = int>
+	template<class T, class index_type = integer_t>
 	struct type_index
 	{
 	private:
-		static index_type& type_counter() {
+		static std::atomic<index_type>& type_counter() {
 			static std::atomic<index_type> counter = 0;
 			return counter;
 		};
 
 	public:
 		template<class S>
-		static int get() {
-			static int index = type_counter().fetch_add(1);
+		static index_type get() {
+			static index_type index = type_counter().fetch_add(1);
 			return index;
 		}
 	};
