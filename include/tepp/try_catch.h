97ec55ab7a55cd1d1e763a9d56643eb97a3b7a6f
Intrets <geen.inspiratie824@gmail.com>
2024-10-19 02:46:13 +0200

    TRY_CATCH_OR_0

--- include/tepp/try_catch.h
+++ include/tepp/try_catch.h
@@ -5,4 +5,6 @@
 namespace te
 {
 #define TRY_CATCH(X) [&] () -> std::optional<decltype(X)> { try { return std::make_optional(X); } catch (std::exception const&){ return std::nullopt; } }()
+
+#define TRY_CATCH_OR_0(X) [&] () -> decltype(X) { try { return X; } catch (std::exception const&){ return {}; } }()
 }
