b438615a65df2c1c10388c7477a846f146c4fe1e
Intrets <geen.inspiratie824@gmail.com>
2025-01-28 19:44:19 +0100

    maybe

---
+++ include/tepp/maybe.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "tepp/optional_ref.h"
+#include <optional>
+
+namespace te
+{
+	template<class F>
+	auto maybe(bool b, F&& f) {
+		using Return = std::invoke_result_t<F>;
+		if constexpr (std::is_reference_v<Return>) {
+			using FullType = optional_ref<std::remove_reference_t<Return>>;
+			if (b) {
+				return FullType(std::invoke(f));
+			}
+			else {
+				return FullType(nullopt);
+			}
+		}
+		else {
+			using FullType = std::optional<std::remove_reference_t<Return>>;
+			if (b) {
+				return std::make_optional(std::invoke(f));
+			}
+			else {
+				return FullType(std::nullopt);
+			}
+		}
+	}
+}
---
+++ maybe.cpp
@@ -0,0 +1,5 @@
+#include "maybe.h"
+
+namespace te
+{
+}
