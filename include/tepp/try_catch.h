03023fe1c7495aaba8dce9a9b75b1efee7844fa2
Intrets <geen.inspiratie824@gmail.com>
2025-02-19 04:56:10 +0100

    TRY_CATCH_REF

--- include/tepp/try_catch.h
+++ include/tepp/try_catch.h
@@ -2,9 +2,12 @@
 
 #include <optional>
 
+#include "tepp/optional_ref.h"
+
 namespace te
 {
-#define TRY_CATCH(X) [&] () -> std::optional<decltype(X)> { try { return std::make_optional(X); } catch (std::exception const&){ return std::nullopt; } }()
+#define TRY_CATCH(X) [&]() -> std::optional<decltype(X)> { try { return std::make_optional(X); } catch (std::exception const&){ return std::nullopt; } }()
+#define TRY_CATCH_REF(X) [&]()  { try { return te::optional_ref(X); } catch (std::exception const&){ return decltype(te::optional_ref(X))(te::nullopt); } }()
 
-#define TRY_CATCH_OR_0(X) [&] () -> decltype(X) { try { return X; } catch (std::exception const&){ return {}; } }()
+#define TRY_CATCH_OR_0(X) [&]() -> decltype(X) { try { return X; } catch (std::exception const&){ return {}; } }()
 }
