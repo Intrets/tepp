0b1bb87c8ed16d9dd8c592e7d42415ad2dfbb929
Intrets <geen.inspiratie824@gmail.com>
2025-03-05 22:20:23 +0100

    forgot files of course

---
+++ include/tepp/trivial_buffer.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <tepp/integers.h>
+#include <tepp/misc.h>
+
+#include <cstdlib>
+
+namespace te
+{
+	template<class T, integer_t alignment = alignof(T)>
+	struct trivial_buffer
+	{
+		T* data = nullptr;
+		integer_t size = 0;
+
+		T& operator[](integer_t i) {
+			return this->data[i];
+		}
+
+		T const& operator[](integer_t i) const {
+			return this->data[i];
+		}
+
+		std::span<T> getSpan() {
+			return std::span(this->data, this->data + size);
+		}
+
+		void fill(T const& t) {
+			std::ranges::fill(this->getSpan(), t);
+		}
+
+		trivial_buffer() = default;
+		trivial_buffer(trivial_buffer&& other) {
+			this->data = other.data;
+			this->size = other.size;
+
+			other.data = nullptr;
+			other.size = 0;
+		}
+		trivial_buffer& operator=(trivial_buffer&& other) {
+			this->free();
+
+			this->data = other.data;
+			this->size = other.size;
+
+			other.data = nullptr;
+			other.size = 0;
+
+			return *this;
+		}
+
+		NO_COPY(trivial_buffer);
+
+		trivial_buffer(integer_t size_) {
+			this->size = size_;
+#ifdef WIN32
+			this->data = std::launder(reinterpret_cast<T*>(_aligned_malloc(size_ * sizeof(T), alignment)));
+#else
+			this->data = std::launder(reinterpret_cast<T*>(std::aligned_alloc(size_ * sizeof(T), alignment)));
+#endif
+
+			std::memset(this->data, 0, size_ * sizeof(T));
+		}
+
+	private:
+		void free() {
+			if (this->data != nullptr && this->size != 0) {
+#ifdef WIN32
+				_aligned_free(this->data);
+#else
+				std::free(this->data);
+#endif
+			}
+		}
+
+	public:
+		~trivial_buffer() {
+			this->free();
+		}
+	};
+}
---
+++ trivial_buffer.cpp
@@ -0,0 +1,5 @@
+#include "tepp/trivial_buffer.h"
+
+namespace te
+{
+}
