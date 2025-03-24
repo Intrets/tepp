a0888ea0dc438c20a2e202d3cc14af2da6bf241e
Intrets <geen.inspiratie824@gmail.com>
2025-03-24 15:25:49 +0100

    added natvis for simple_vector and isize now also gets member variable

--- include/tepp/integers.h
+++ include/tepp/integers.h
@@ -8,9 +8,19 @@
 using integer_t = std::ptrdiff_t;
 using qualifier_t = int64_t;
 
-template<class I = integer_t>
-inline I isize(auto&& a) {
-	return static_cast<I>(a.size());
+template<class T>
+concept has_size_function = requires(T t) {
+	{ t.size() } -> std::integral;
+};
+
+template<class I = integer_t, class T>
+inline I isize(T const& a) {
+	if constexpr (has_size_function<T>) {
+		return static_cast<I>(a.size());
+	}
+	else {
+		return static_cast<I>(a.size);
+	}
 }
 
 constexpr inline integer_t operator""_i(unsigned long long i) {
