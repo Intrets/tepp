8a6be5c8661f8230b4018e6da1ee7911647acf8e
Intrets <geen.inspiratie824@gmail.com>
2025-03-28 17:28:35 +0100

    more hashing helper functions

--- include/tepp/hash.h
+++ include/tepp/hash.h
@@ -2,6 +2,7 @@
 
 #include <bit>
 #include <span>
+#include <type_traits>
 
 #include <tepp/tepp.h>
 
@@ -39,6 +40,47 @@ namespace te
 			);
 
 			return static_cast<size_t>(hash);
+		}
+	};
+
+	struct hasher
+	{
+		// FNV-1
+		static constexpr uint64_t prime = 1099511628211ULL;
+		static constexpr uint64_t offset = 14695981039346656037ULL;
+
+		uint64_t hash = offset;
+
+		operator size_t() const {
+			return static_cast<size_t>(this->hash);
+		}
+
+		template<detail::no_padding T>
+		hasher& add(std::span<T> data) {
+			for (auto byte : std::as_bytes(data)) {
+				hash *= prime;
+				hash ^= static_cast<uint64_t>(std::bit_cast<uint8_t>(byte));
+			}
+
+			return *this;
+		}
+
+		template<detail::no_padding... T>
+		hasher& add(T const&... data) {
+			te::for_each(
+			    [&](auto const& data) {
+				    auto constexpr byteSize = sizeof(data);
+				    std::byte const* raw = reinterpret_cast<std::byte const*>(&data);
+
+				    for (auto byte : std::span<std::byte const>(raw, byteSize)) {
+					    this->hash *= prime;
+					    this->hash ^= static_cast<uint64_t>(std::bit_cast<uint8_t>(byte));
+				    }
+			    },
+			    data...
+			);
+
+			return *this;
 		}
 	};
 
