344fbe4c7a167ed18158e162698514f09b2de218
Intrets <geen.inspiratie824@gmail.com>
2025-04-10 03:24:28 +0200

    scale_range

--- include/tepp/enumerate.h
+++ include/tepp/enumerate.h
@@ -2,9 +2,15 @@
 
 #include <ranges>
 
+#include "tepp/integers.h"
+
 namespace te
 {
-	inline constexpr auto enumerate_range = [](auto&& range){
+	inline constexpr auto enumerate_range = [](auto&& range) {
 		return std::views::zip(std::views::iota(0_i), range);
+	};
+
+	inline constexpr auto scale_range = [](auto&& range) {
+		return std::views::zip(std::views::iota(0_i) | std::views::transform([&](auto i) { return static_cast<float>(i) / isize(range); }), range);
 	};
 }
