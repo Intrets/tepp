56f0daf03c32e75967782c061bab4cf90c1ffa05
Intrets <geen.inspiratie824@gmail.com>
2025-05-05 02:08:01 +0200

    optional unwrapping works with std::optional too

--- include/tepp/optional.h
+++ include/tepp/optional.h
@@ -6,10 +6,73 @@
 
 #define UNWRAP_OPTIONAL(X, Y, Z) \
 	if (auto Y##optional123456789 = Z) \
-		for (X Y : Y##optional123456789)
+		for (X Y : te::detail::wrap(Y##optional123456789))
 
 namespace te
 {
+	namespace detail
+	{
+		template<class T>
+		struct iterator_wrapper
+		{
+			std::optional<T>& data;
+
+			T const* begin() const {
+				if (this->data.has_value()) {
+					return &this->data.value();
+				}
+				else {
+					return nullptr;
+				}
+			}
+
+			T const* end() const {
+				if (this->data.has_value()) {
+					return &this->data.value() + 1;
+				}
+				else {
+					return nullptr;
+				}
+			}
+
+			T* begin() {
+				if (this->data.has_value()) {
+					return &this->data.value();
+				}
+				else {
+					return nullptr;
+				}
+			}
+
+			T* end() {
+				if (this->data.has_value()) {
+					return &this->data.value() + 1;
+				}
+				else {
+					return nullptr;
+				}
+			}
+		};
+
+		template<class T>
+		concept is_std_optional = requires(T t) {
+			typename T::value_type;
+			std::same_as<std::optional<typename T::value_type>, T>;
+		};
+
+		template<class T_>
+		decltype(auto) wrap(T_&& t) {
+			using T = std::remove_cvref_t<T_>;
+
+			if constexpr (is_std_optional<T>) {
+				return iterator_wrapper<typename T::value_type>{ .data = t };
+			}
+			else {
+				return t;
+			}
+		}
+	}
+
 	template<class T>
 	struct optional
 	{
