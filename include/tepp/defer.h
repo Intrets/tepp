e04a5575840cef002e4da4ab9c8679c13c3c03b0
Intrets <geen.inspiratie824@gmail.com>
2025-05-05 13:09:26 +0200

    improved defer safety

--- include/tepp/defer.h
+++ include/tepp/defer.h
@@ -4,17 +4,31 @@
 
 namespace te
 {
-	template<class F>
-	struct defer
+	namespace detail
 	{
-		F f;
+		template<class F>
+		struct defer
+		{
+			F f;
 
-		defer() = delete;
-		defer(F&& f_)
-		    : f(std::forward<F>(f_)) {
-		}
-		~defer() {
-			this->f();
-		}
-	};
+			defer(defer&&) = delete;
+			defer& operator=(defer&&) = delete;
+
+			defer(defer const&) = delete;
+			defer& operator=(defer const&) = delete;
+
+			defer() = delete;
+			defer(F&& f_)
+			    : f(std::forward<F>(f_)) {
+			}
+			~defer() {
+				this->f();
+			}
+		};
+	}
+
+	template<class F>
+	[[nodiscard]] detail::defer<F> defer(F&& f) {
+		return detail::defer<F>(std::forward<F>(f));
+	}
 }
