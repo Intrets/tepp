41147a4fafe3abed7ce067d1f937deb418928b00
Intrets <geen.inspiratie824@gmail.com>
2025-05-05 13:44:56 +0200

    structs can be nodiscard

--- include/tepp/defer.h
+++ include/tepp/defer.h
@@ -4,31 +4,23 @@
 
 namespace te
 {
-	namespace detail
-	{
-		template<class F>
-		struct defer
-		{
-			F f;
-
-			defer(defer&&) = delete;
-			defer& operator=(defer&&) = delete;
-
-			defer(defer const&) = delete;
-			defer& operator=(defer const&) = delete;
-
-			defer() = delete;
-			defer(F&& f_)
-			    : f(std::forward<F>(f_)) {
-			}
-			~defer() {
-				this->f();
-			}
-		};
-	}
-
 	template<class F>
-	[[nodiscard]] detail::defer<F> defer(F&& f) {
-		return detail::defer<F>(std::forward<F>(f));
-	}
+	struct [[nodiscard]] defer
+	{
+		F f;
+
+		defer(defer&&) = delete;
+		defer& operator=(defer&&) = delete;
+
+		defer(defer const&) = delete;
+		defer& operator=(defer const&) = delete;
+
+		defer() = delete;
+		defer(F&& f_)
+		    : f(std::forward<F>(f_)) {
+		}
+		~defer() {
+			this->f();
+		}
+	};
 }
