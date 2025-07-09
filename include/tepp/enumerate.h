abfb1cf77767b47dc3616c93a05eabf9d48e5fee
Intrets <geen.inspiratie824@gmail.com>
2025-07-09 21:17:39 +0200

    scale_range goes from 0 to 1 inclusively.

--- include/tepp/enumerate.h
+++ include/tepp/enumerate.h
@@ -11,6 +11,6 @@ namespace te
 	};
 
 	inline constexpr auto scale_range = [](auto&& range) {
-		return std::views::zip(std::views::iota(0_i) | std::views::transform([=](auto i) { return static_cast<float>(i) / isize(range); }), range);
+		return std::views::zip(std::views::iota(0_i) | std::views::transform([=](auto i) { return static_cast<float>(i) / (isize(range) - 1); }), range);
 	};
 }
