778988803970d8e90676cea75cbdaa6933164cb8
Intrets <geen.inspiratie824@gmail.com>
2025-07-30 23:08:10 +0200

    explicit functions for checking floating point division

---
+++ floating_point.cpp
@@ -0,0 +1,5 @@
+#include "tepp/floating_point.h"
+
+namespace te
+{
+}
---
+++ include/tepp/floating_point.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <concepts>
+
+namespace te
+{
+	// Check if (1/f)^2 results in finite value. Squared to have some headroom.
+	template<std::floating_point F>
+	constexpr bool floating_division_check(F f) {
+		if constexpr (std::same_as<F, float>) {
+			return std::abs(f) > 1e-19;
+		}
+		else if constexpr (std::same_as<F, double>) {
+			return std::abs(f) > 1e-150;
+		}
+	}
+
+	// Check if (1/f)^2 results in finite value. Squared to have some headroom.
+	template<std::floating_point F>
+	constexpr bool floating_zero_check(F f) {
+		return floating_division_check(f);
+	}
+}
