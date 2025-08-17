a634e9789100017d434cca6f1979db2e1cf4affd
Intrets <azarintrets@live.nl>
2025-08-17 02:23:28 +0200

    get_future<future<void>> specialization, and handling broken promise

--- include/tepp/future.h
+++ include/tepp/future.h
@@ -11,10 +11,28 @@ namespace te
 	template<class T>
 	using promise = std::promise<T>;
 
-	template<class T>
-	te::optional<T> get_future(future<T>& f) {
+	inline bool get_future(future<void>& f) {
 		if (f.valid() && f.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
-			return f.get();
+			try {
+				f.get();
+				return true;
+			} catch (std::exception&) {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+
+	template<class T>
+	inline te::optional<T> get_future(future<T>& f) {
+		if (f.valid() && f.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+			try {
+				return f.get();
+			} catch (std::exception&) {
+				return std::nullopt;
+			}
 		}
 		else {
 			return std::nullopt;
