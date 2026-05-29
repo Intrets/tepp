285cc5dc22d95035e02f68258b1613386e4e1c11
Intrets <azarintrets@live.nl>
2026-05-29 16:44:01 +0200

    functional update

--- functional.h
+++ functional.h
@@ -28,8 +28,29 @@ namespace te
 
 	template<class F>
 	auto copy(F&& f) {
-		return [=](auto&& val) {
-			return auto(std::invoke(f, std::forward<decltype(val)>(val)));
+		return [=](auto&& val) -> std::remove_cvref_t<std::invoke_result_t<F, decltype(val)>> {
+			return std::invoke(f, std::forward<decltype(val)>(val));
+		};
+	}
+
+	template<class T>
+	concept can_ref = requires(T t) {
+		std::ref(t);
+	};
+
+	auto ref_if_possible(auto&& a) {
+		if constexpr (can_ref<decltype(a)>) {
+			return std::ref(a);
+		}
+		else {
+			return a;
+		}
+	}
+
+	template<class... Fs>
+	auto extract(Fs&&... fs) {
+		return [=](auto&& e) {
+			return std::tuple<std::invoke_result_t<Fs, decltype(e)>...>(std::invoke(fs, e)...);
 		};
 	}
 }
