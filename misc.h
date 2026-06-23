ebb635ab0861f2d2cc9360688f5a27901c23bdfa
Intrets <9140942+Intrets@users.noreply.github.com>
2026-06-24 00:50:40 +0200

    sanitizer macros

--- misc.h
+++ misc.h
@@ -22,3 +22,14 @@
 #define DEFAULT_COPY_MOVE(T) DEFAULT_COPY(T) DEFAULT_MOVE(T)
 #define NO_COPY_MOVE(T) NO_COPY(T) NO_MOVE(T)
 
+#if defined(COMPILER_CLANG) || defined(COMPILER_CLANGCL)
+#define HAS_ADDRESS_SANITIZER __has_feature(address_sanitizer)
+#define HAS_THREAD_SANITIZER __has_feature(thread_sanitizer)
+#define HAS_UNDEFINED_SANITIZER __has_feature(undefined_sanitizer)
+#elifdef COMPILER_MSVC
+#define HAS_ADDRESS_SANITIZER __SANITIZE_ADDRESS__
+#define HAS_THREAD_SANITIZER __SANITIZER_THREAD__
+#define HAS_UNDEFINED_SANITIZER 0
+#else
+#error unsupported compiler
+#endif
